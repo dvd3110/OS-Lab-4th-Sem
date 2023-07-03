83735de017f3b7d247deefa8ebf22a103d8c4d77
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2023-07-03 13:09:08 +0530

    Update RoundRobin.c

--- RoundRobin.c
+++ RoundRobin.c
@@ -11,13 +11,16 @@ int main()
         printf("\n Enter the Arrival and Burst time of the Process[%d]: ", i + 1);
         scanf("%d", &at[i]);
         scanf("%d", &bt[i]);
-        temp[i] = bt[i]; // store the burst time in temp array
+        temp[i] = bt[i];
     }
+
     printf("Enter the time quantum for the process: \t");
     scanf("%d", &quant);
+
     printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");
     for (sum = 0, i = 0; y != 0;)
     {
+        // condition 1
         if (temp[i] <= quant && temp[i] > 0)
         {
             sum = sum + temp[i];
@@ -29,6 +32,8 @@ int main()
             temp[i] = temp[i] - quant;
             sum = sum + quant;
         }
+
+        // condition 2
         if (temp[i] == 0 && count == 1)
         {
             y--;
@@ -37,11 +42,9 @@ int main()
             tat = tat + sum - at[i];
             count = 0;
         }
-        if (i == n - 1)
-        {
-            i = 0;
-        }
-        else if (at[i + 1] <= sum)
+
+        // condition 3
+        if (at[i + 1] <= sum)
         {
             i++;
         }
@@ -50,10 +53,12 @@ int main()
             i = 0;
         }
     }
+
     float avg_wt, avg_tat;
     avg_wt = wt * 1.0 / n;
     avg_tat = tat * 1.0 / n;
     printf("\n Average Turn Around Time: \t%f", avg_wt);
-    printf("\n Average Waiting Time: \t%f", avg_tat);
+    printf("\n Average Waiting Time: \t%f\n", avg_tat);
+
     return 0;
 }
